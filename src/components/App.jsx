d08b962adfcf10db37579d2125abe16b77868c32
Vita <v.mars22@ukr.net>
2024-04-17 16:03:47 +0300

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,32 +6,33 @@ import viteLogo from '/vite.svg' */
 import { useState, useEffect } from 'react';
 import ContactList from './ContactList.jsx';
 import ContactForm from './ContactForm.jsx';
-import SearchBox from './Searchbox.jsx';
+import SearchBox from './SearchBox.jsx'
 import css from './App.module.css'
 
 
 
 
 
-function App() {
+export default function App() {
 
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [value, setValues] = useState(() => {
+  const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : "";
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
   });
+  const [value, setValue] = useState('');
+
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const handleChange = (event) => {
-    setValues(event.target.value.toString());
+    setValue(event.target.value);
   };
 
   const addContact = (newContact) => {
@@ -61,6 +62,3 @@ function App() {
     </>
   )
 }
-
-export default App
- 
